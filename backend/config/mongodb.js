d3220d1d1c0bed6ff3a8e171b114bb8aa0cdef1a
ramchandra-siddh <ramchandrasiddh18@gmail.com>
2025-06-22 20:05:20 +0530

    backend Modify

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,14 +1,7 @@
 import mongoose from "mongoose";
 const connetDB = async () => {
-  try {
-    mongoose.connection.on("connected", () =>
-      console.log("Database Connected")
-    );
-    await mongoose.connect(`${process.env.MONGODB_URI}/healthgrid`);
-  } catch (err) {
-    console.error("MongoDB connection failed:", err.message);
-    process.exit(1);
-  }
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
+  await mongoose.connect(`${process.env.MONGODB_URI}/healthgrid`);
 };
 
 export default connetDB;
