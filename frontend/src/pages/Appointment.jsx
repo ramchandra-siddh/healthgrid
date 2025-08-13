9c280eacfc706b207e4a713849a7335cd6ebcb7f
ramchandra-siddh <ramchandrasiddh18@gmail.com>
2025-08-13 22:06:05 +0530

    small bug

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -152,8 +152,8 @@ const Appointment = () => {
           <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
             {/*-------- Doc Info : name, degree, experience */}
             <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
-              {docInfo.name}{" "}
-              <img className="w-5" src={assets.verified_icon} alt="" />{" "}
+              {docInfo.name}
+              <img className="w-5" src={assets.verified_icon} alt="" />
             </p>
             <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
               <p>
