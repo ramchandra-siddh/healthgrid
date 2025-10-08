aa2987160765773dc6543f21b0092a9dc00fd752
ramchandra-siddh <ramchandrasiddh18@gmail.com>
2025-10-08 15:53:58 +0530

    email verification

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,26 +1,28 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import "dotenv/config";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  logger: true,
-  debug: true,
-});
+const resend = new Resend(process.env.RESEND_HEALTHGRID_API_KEY);
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   logger: true,
+//   debug: true,
+// });
 
 async function sendMail({ to, subject, html }) {
   try {
-    const info = await transporter.sendMail({
-      from: `"HealthGrid" <${process.env.EMAIL_USER}>`,
+    const info = await resend.emails.send({
+      from: "HealthGrid <onboarding@resend.dev>",
       to,
       subject,
       html,
     });
 
-    console.log("Email sent:", info.messageId);
+    console.log("Email sent:", info?.id || "No ID");
     return true;
   } catch (err) {
     console.error("Email error:", err);
